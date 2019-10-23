d19a7a4707eb31e003a3a08145865ab5cbb5bd3f
akshayjain3450 <akshayjain3450@gmail.com>
2019-10-23 11:31:06 +0530

    Assignment 5

---
+++ Course 2/ass5.py
@@ -0,0 +1,14 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : 
+	fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+lst = list()
+for line in fh:
+	if line.startswith("From ") == True:
+		lst = line.split()
+		print(lst[1])
+		count = count + 1
+		
+print("There were", count, "lines in the file with From as the first word")
