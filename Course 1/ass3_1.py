2fc12cc1538fb5246239dfeb92a8babac1dac6ab
akshayjain3450 <akshayjain3450@gmail.com>
2019-10-23 11:25:47 +0530

    Assignment 3 Part 1

---
+++ Course 1/ass3_1.py
@@ -0,0 +1,16 @@
+s = input("Enter Score: ")
+try:
+	score = float(s)
+	if score >=0 and score <= 1.0:
+		if score >= 0.9:
+			print("A")
+		elif score >= 0.8:
+			print("B")
+		elif score >= 0.7:
+			print("C")
+		elif score >= 0.6:
+			print("D")
+		else:
+			print("F")
+except:
+	print("Error")
