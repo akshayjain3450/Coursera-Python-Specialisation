1a971a11f8482e07ffcd2babc92f5aaf6ec40b0c
akshayjain3450 <akshayjain3450@gmail.com>
2019-10-26 21:57:47 +0530

    Visualization Script

---
+++ Course 5/gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Year','umich.edu','indiana.edu','cam.ac.uk','gmail.com','uct.ac.za','berkeley.edu','mac.com','unicon.net','ucdavis.edu','stanford.edu'],
+['2005',56,12,7,10,14,12,11,6,11,4],
+['2006',1265,618,584,421,421,396,396,383,296,259],
+['2007',457,275,260,227,204,177,74,53,125,154]
+];
